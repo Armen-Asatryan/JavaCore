9c852bdc2b415f1610cdb1c2bb35bf1482778c86
Armen Asatryan <armensas12345@gmail.com>
2023-08-25 16:55:36 +0400

    homework update

---
+++ src/homework/work3/AutoArray.java
@@ -0,0 +1,8 @@
+package homework.work3;
+
+public class AutoArray {
+    public static void main(String[] args) {
+        int month_days[] = {31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30,};
+        System.out.println("April " + month_days[3] + " days");
+    }
+}
