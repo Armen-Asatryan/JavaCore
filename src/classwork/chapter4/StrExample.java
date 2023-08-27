d92f19ecda4e482a564dfe9c5bd6cfeffc6ea42c
Armen Asatryan <armensas12345@gmail.com>
2023-08-28 00:32:46 +0400

    homework update

---
+++ src/classwork/chapter4/StrExample.java
@@ -0,0 +1,12 @@
+package classwork.chapter4;
+
+public class StrExample {
+    public static void main(String[] args) {
+        String name = "Hakob";
+       char[] chars = name.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            System.out.print(chars[i] + " ");
+
+        }
+    }
+}
