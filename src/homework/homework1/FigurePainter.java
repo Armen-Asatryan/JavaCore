75c3ac92f9c5206c00974fc019574f010a2cdb41
Armen Asatryan <armensas12345@gmail.com>
2023-08-28 17:37:56 +0400

    classwork update

--- src/homework/homework1/FigurePainter.java
+++ src/homework/homework1/FigurePainter.java
@@ -2,6 +2,8 @@ package homework.homework1;
 
 public class FigurePainter {
     public static void main(String[] args) {
+
+
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
@@ -16,13 +18,14 @@ public class FigurePainter {
                 System.out.print("* ");
 
             }
-            System.out.println();
+            System.out.println( );
 
 
         }
-
     }
 }
+
+
 
 
 
