7dc47055496660146c64ebff8a546699d70b24b4
Armen Asatryan <armensas12345@gmail.com>
2023-08-28 18:10:45 +0400

    classwork update

--- src/homework/homework1/FigurePainter.java
+++ src/homework/homework1/FigurePainter.java
@@ -22,6 +22,53 @@ public class FigurePainter {
 
 
         }
+       int a = 5;
+        for (int i = 1; i <=a;i++) {
+            for (int j = i; j < a; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        int b = 5;
+        for (int i = 1; i <=b;i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j <= b; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        int d = 5;
+        for (int i = 1; i <=d;i++) {
+            for (int j = i; j < d; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" *");
+
+            }
+            System.out.println();
+
+        }
+        int c = 5;
+        for (int i = 1; i <=c;i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= c; j++) {
+                System.out.print(" *");
+
+            }
+            System.out.println();
+        }
     }
 }
 
