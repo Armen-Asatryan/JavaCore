2e1cdcca4868b7dfc69c91f552f9eb2b69b58969
Armen Asatryan <armensas12345@gmail.com>
2023-09-12 21:29:27 +0400

    homework update

---
+++ src/homework/homework4/Sort.java
@@ -0,0 +1,26 @@
+package homework.homework4;
+
+import java.util.Arrays;
+
+public class Sort {
+
+    public static void main(String[] args) {
+        int[] array = {4, 7, 1, 3, 9, 0, 2};
+        int value = 0;
+        for (int j = 0; j < array.length; j++) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i + 1] < array[i]) {
+                    value = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = value;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+
+        }
+    }
+}
+
+
