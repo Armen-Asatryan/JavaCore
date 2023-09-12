1702cc8bb91f359098357e179c3299a305eba1e1
Armen Asatryan <armensas12345@gmail.com>
2023-09-12 13:45:56 +0400

    classwork update

---
+++ src/classwork/chapter9/Box.java
@@ -0,0 +1,7 @@
+package classwork.chapter9;
+
+public class Box {
+    double width;
+    double height;
+    double depth;
+}
---
+++ src/classwork/chapter9/Boxtest.java
@@ -0,0 +1,13 @@
+package classwork.chapter9;
+
+public class Boxtest {
+    public static void main(String[] args) {
+        Box myBox = new Box();
+        myBox.depth = 4.4;
+        System.out.println(myBox.depth);
+        System.out.println(myBox.height);
+        System.out.println(myBox.width);
+
+
+    }
+}
