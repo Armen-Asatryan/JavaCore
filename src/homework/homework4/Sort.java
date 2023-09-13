3cbb3af1f7b4d8d2ddb4035a5f877c7710b6282a
Armen Asatryan <armensas12345@gmail.com>
2023-09-13 12:14:57 +0400

    homework update

--- src/homework/homework4/Sort.java
+++ src/homework/homework4/Sort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Sort {
 
     public static void main(String[] args) {
-        int[] array = {4, 7, 1, 3, 9, 0, 2};
+        int[] array = {4, 7, 1, 3, 9, 0, 2, 14, 6, 22};
         int value = 0;
         for (int j = 0; j < array.length; j++) {
             for (int i = 0; i < array.length - 1; i++) {
