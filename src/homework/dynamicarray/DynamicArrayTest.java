2717edaa1c64173d62c328e8d644d0254f8452ea
Armen Asatryan <armensas12345@gmail.com>
2023-09-21 21:09:13 +0400

    homework update

---
+++ src/homework/dynamicarray/DynamicArrayTest.java
@@ -0,0 +1,15 @@
+package homework.dynamicarray;
+
+public class DynamicArrayTest {
+    public static void main(String[] args) {
+        DynamicArray dy = new DynamicArray();
+        dy.add(2);
+        dy.add(2);
+        dy.add(2);
+        dy.add(2);
+        dy.add(2);
+        dy.add(2);
+        dy.print();
+
+    }
+}
