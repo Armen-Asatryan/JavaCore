bb3c29c56f8b9a0be20a865d9da28133172f11c1
Armen Asatryan <armensas12345@gmail.com>
2023-09-26 00:28:32 +0400

    homework update

--- src/homework/dynamicarray/DynamicArray.java
+++ src/homework/dynamicarray/DynamicArray.java
@@ -20,7 +20,7 @@ public class DynamicArray {
     }
 
     public int getByIndex(int index) {
-        if (index<0 || index >= size){
+        if (index < 0 || index >= size) {
             System.out.println("Wrong index");
             return 0;
         }
@@ -31,6 +31,61 @@ public class DynamicArray {
         for (int i = 0; i < size; i++) {
             System.out.print(array[i] + " ");
         }
+        System.out.println();
+    }
+
+    public void deleteByIndex(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Wrong index");
+            return;
+
+        }
+        for (int i = index + 1; i < size; i++) {
+            array[i - 1] = array[i];
+        }
+        size--;
+    }
+
+    public void set(int index, int value) {
+        if (index < 0 || index >= size) {
+            System.out.println("Wrong index");
+
+        }
+        array[index] = value;
+    }
+
+    public void add(int index, int value) {
+        if (index < 0 || index >= size) {
+            System.out.println("Wrong index");
+        }
+        if (size == array.length) {
+            extend();
+        }
+        for (int i = size; i > index; i--) {
+            array[i + 1] = array[i];
+        }
+        array[index] = value;
+        size++;
+
+    }
+
+    public boolean exists(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return true;
+
+            }
+        }
+        return false;
+    }
+    int getIndexByvalue(int value){
+        for (int i = 0; i < size; i++){
+            if (array[i]== value)
+                return i;
+        }
+        return -1;
     }
 }
+
+
 
--- src/homework/dynamicarray/DynamicArrayTest.java
+++ src/homework/dynamicarray/DynamicArrayTest.java
@@ -3,13 +3,15 @@ package homework.dynamicarray;
 public class DynamicArrayTest {
     public static void main(String[] args) {
         DynamicArray dy = new DynamicArray();
+        dy.add(1);
         dy.add(2);
-        dy.add(2);
-        dy.add(2);
-        dy.add(2);
-        dy.add(2);
-        dy.add(2);
+        dy.add(3);
+        dy.add(4);
+        dy.add(5);
         dy.print();
+        dy.deleteByIndex(4);
+        dy.print();
+        System.out.println(dy.exists(6));
 
     }
 }
