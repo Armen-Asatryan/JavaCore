84235554b951f0baf6f8273b55df3287b7a31b0a
Armen Asatryan <armensas12345@gmail.com>
2023-09-30 22:43:24 +0400

    homework update

---
+++ src/classwork/chapter11/VarArgs1.java
@@ -0,0 +1,36 @@
+package classwork.chapter11;
+
+public class VarArgs1 {
+    static void vaTest(int... v) {
+        System.out.println("vaTest(int...):" + "Количество аргументов:" + v.length + "Содержимое: ");
+        for (int x : v) {
+            System.out.println(x + " ");
+            System.out.println();
+        }
+    }
+
+    static void vaTest(boolean... v) {
+        System.out.print("vaTest(boolean ... ) " +
+                "Количество аргументов: "
+                + v.length + "Содержимое: ");
+        for (boolean x : v) {
+            System.out.println(x + " ");
+            System.out.println();
+        }
+    }
+
+    static void vaTest(String msg, int... v) {
+        System.out.print("vaтest(String, int ... ) : " +
+                msg + v.length + "Содержимое: ");
+        for (int x : v) {
+            System.out.println(x + " ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        vaTest(1, 4, 7 );
+        vaTest(10,20);
+        vaTest(true,false,false);
+    }
+}
